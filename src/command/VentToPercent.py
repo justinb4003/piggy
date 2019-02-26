d6014d1eb787e4da765d614b0bef9e24e0a31e7b
Justin Buist <justin.buist@gmail.com>
2019-02-25 21:52:30 -0500

    Code cleanup.

--- src/command/VentToPercent.py
+++ src/command/VentToPercent.py
@@ -1,30 +1,26 @@
 from .BaseCommand import BaseCommand
-from subsystem.Vent import Vent 
+
 
 class VentToPercent(BaseCommand):
-	#vent = Vent("Roof vent", 2, 4, 30)
 
-	def set_vent(self, vent):
-		self.vent = vent
+    def set_vent(self, vent):
+        self.vent = vent
 
-	def set_target(self, target):
-		self.target_pct = target
+    def set_target(self, target):
+        self.target_pct = target
 
-	def execute(self):
-		# There should probably be some ramping here but that'll
-		# depend entirely on the controller.`
-		if (self.vent.get_percent() < self.target_pct):
-			self.vent.set_open()
-		else:
-			self.vent.set_close()
+    def execute(self):
+        # There should probably be some ramping here but that'll
+        # depend entirely on the controller.`
+        if (self.vent.get_percent() < self.target_pct):
+            self.vent.set_open()
+        else:
+            self.vent.set_close()
 
-	def is_finished(self):
-		if ( abs(self.vent.get_percent() - self.target_pct) < 0.20 ):
-			return True
-		return False
+    def is_finished(self):
+        if (abs(self.vent.get_percent() - self.target_pct) < 0.20):
+            return True
+        return False
 
-	def end(self):
-		self.vent.stop()
-		
-	
-
+    def end(self):
+        self.vent.stop()
