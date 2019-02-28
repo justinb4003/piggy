0d98f0c92863ae01b21f90bd4fb902b44ad1396c
Justin Buist <justin.buist@gmail.com>
2019-02-27 22:21:39 -0500

    That completes the basic cooling task unless I need to revamp how I'm struction all of this... which is very possible.  At least where it stands it all works.

--- src/task/Cooling.py
+++ src/task/Cooling.py
@@ -10,12 +10,10 @@ class Cooling(BaseTask):
     temp_sensor = eqfetch.get_temp("TEMP01")
     vent1 = eqfetch.get_vent("RETROOF")
 
-    setpoint = 80
     crack = 8
     step = 3
-    on_offset = 3
-    off_offset = 0
-    priority = 0
+    on_at = 82
+    off_at = 78
 
     def __init__(self, name, priority):
         self.name = name
@@ -28,18 +26,14 @@ class Cooling(BaseTask):
         return self._action(False)
 
     def get_priority(self):
-        return self.priority
-
-    def set_priority(self, val):
-        self.priority = val
+        return 0
 
     def export_dict(self):
         d = {}
         d['name'] = self.name
         d['type'] = type(self).__name__
-        d['setpoint'] = self.setpoint
-        d['on_offset'] = self.on_offset
-        d['off_offset'] = self.off_offset
+        d['on_at'] = self.on_at
+        d['off_at'] = self.off_at
         d['crack'] = self.crack
         d['step]'] = self.step
         d['want_action'] = self.want_action()
@@ -54,59 +48,53 @@ class Cooling(BaseTask):
     def _action(self, doit):
         ret_val = False
         temp = self.temp_sensor.get_temp()
-        temp_on = self.setpoint + self.on_offset
-        temp_off = self.setpoint + self.off_offset
-        print("COOLING________")
-        print("setpoint: " + str(self.setpoint))
-        print("on offset: " + str(self.on_offset))
-        print("off offset: " + str(self.off_offset))
+        pct = self.vent1.get_percent()
+        vent = self.vent1
+        print("COOLING:")
+        print("on at: " + str(self.on_at))
+        print("off at: " + str(self.off_at))
         print("temp: " + str(temp))
-        print("temp_on:" + str(temp_on))
-        print("temp_off:" + str(temp_off))
-        print("vent1 is currently: " + str(self.vent1.get_percent()))
+        print("vent1 is currently: " + str(pct))
 
-        vent_is_open = False
-        if (self.vent1.get_percent() >= 0.00):
-            vent_is_open = True
+        new_pct = pct
+        if temp >= self.on_at and pct <= 0:
+            # If we need to open but it's the first move we only go the
+            # 'crack' positon.
+            new_pct = self.crack
+        if temp >= self.on_at and pct > 0:
+            # If we need to open but we're already partly open we just move
+            # up another 'step'
+            new_pct = pct + self.step
+        if temp <= self.off_at:
+            # If our temp dropped below the off point we slam them shut
+            # pronto.
+            new_pct = -1
 
-        if temp <= temp_off:
-            print("temp <= temp_off")
-            # If we're below the off temp for cooling then we try and move
-            # the vents to -1.00% anyway.
-            ret_val = True
-            if doit:
-                vtp = VentToPercent()
-                vtp.set_vent(self.vent1)
-                vtp.set_target(-1.00)
-                shd.add_sequential(vtp)
-            return  # Nothing else gets evaluated.  Vents are slamming closed.
+        # THINK: Is this where we should be checking if a subsystem can
+        # actually take a command?  I'm not sure who's job that should be.
+        #
+        # If you leave it up the task the worst that can happen is you have
+        # actions queued up that may not really be necessary.  For instance
+        # before this commit the vent would always be targeting one extra
+        # 'step' ahead from where it could actually get.
+        #
+        # It might be nice if there was an official contract for whether
+        # or not a subsystem can take a command to it.
+        #
+        # I don't think the rejection of commands should be left to the
+        # scheduler right now.  Leaving it at the task level allows any
+        # task to take priority over the rest of the system.  That sounds
+        # dangerous but ultimately I want the tasks to be simple and easy
+        # to understand.  That'll make complex programming tasks easier to
+        # implement.  I hope.  It could also end up a nightmare.  We'll see.
+        if vent.can_move() is False:
+            new_pct = pct
 
-        if temp >= temp_on and vent_is_open is False:
-            print("temp >= temp_on and vent_is_open is False")
-            # we're just opening the vents from a closed position.
-            ret_val = True
-            if doit:
-                vtp = VentToPercent()
-                vtp.set_vent(self.ven1)
-                vtp.set_target(self.crack)
-                shd.add_sequential(vtp)
-
-        if vent_is_open and temp > self.setpoint:
-            print("vent_is_open and temp > self.setpoint")
-            ret_val = True
-            if doit:
-                vtp = VentToPercent()
-                vtp.set_vent(self.vent1)
-                vtp.set_target(self.vent1.get_percent() + self.step)
-                shd.add_sequential(vtp)
-
-        if vent_is_open and temp < self.setpoint:
-            print("vent_is_open and temp < self.setpoint")
-            ret_val = True
-            if doit:
-                vtp = VentToPercent()
-                vtp.set_vent(self.vent1)
-                vtp.set_target(self.vent1.get_percent() - self.step)
-                shd.add_sequential(vtp)
+        if new_pct != pct:
+            print("Setting vents to new percent: {}".format(str(new_pct)))
+            vtp = VentToPercent()
+            vtp.set_vent(vent)
+            vtp.set_target(new_pct)
+            shd.add_sequential(vtp)
 
         return ret_val
