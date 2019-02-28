715169bb691b735e419c364b7ce40ea339a01dfe
Justin Buist <justin.buist@gmail.com>
2019-02-27 22:15:54 -0500

    Better commenting to explain the system.

--- src/main.py
+++ src/main.py
@@ -13,22 +13,50 @@ print('starting')
 
 
 def scheduler_loop():
+    """
+    The sheduler loop runs Commands that interface with hardware.
+    Anything that would require controlling equipment for a period of
+    time should issue a Command that can go into the scheduler.
+    """
     while True:
         shd.execute()
         time.sleep(0.10)
 
 
 def task_loop():
+    """
+    Tasks figure out how to manipulate the equipment to achieve a goal.
+    So, 'Heating' can be a task that just runs heater(s), A 'Cooling' task
+    can run roof vents or side vents.  A 'Shading' task could control a heat
+    curtain for shade, etc.
+
+    This is what is going to make this project very flexible.  No task will
+    actually be required by the system.  You could run it without any loaded
+    at all but nothing would happen.  Users will be able to remove factory
+    installed tasks entirely from the disk and if they want upload their own
+    custom ones.  This way instead of having configuration options that your
+    team has to remember you don't use, or remember not to turn on some
+    features, you can simply remove them entirely.
+
+    TODO: Create a configuration interface for tasks so you can configure
+    them via a UI 'mad-lib' style.  Ie: When [temp_sensor] is over [num] deg
+    activate [heater].
+    """
     tr.load_tasks()
     while True:
         tr.execute()
-        time.sleep(30)
+        # 5 second sleep is WAY too fast for real life but nice for
+        # testing.
+        time.sleep(5)
 
 
 # We'll let the controller loop stop the scheduler, insert commands, etc.
 # It'll have a wide berth in what it can do instead of making the scheduler
 # figure that out.  This lets us load up a remote UI with a lot of control.
 def controller_loop():
+    """
+    Yeah haven't really done much with this yet.
+    """
     rmc.init()
     while True:
         raw_command = rmc.listenForCommand()
@@ -54,5 +82,7 @@ def start_threads():
     rest_thread.start()
 
 
+# Load all equipment from database
 eqfetch.load_all()
+# Fire up all of our worker threads
 start_threads()
