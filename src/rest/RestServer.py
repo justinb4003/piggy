0cddbb44d08f1faa00913c7cf638ac0e287a3803
justinb4003 <justin.buist@gmail.com>
2019-03-17 22:19:54 -0400

    Added finer grained methods for tasks, equipment, and sensors. Refactored it a bit and added everything in.  Working here exposed multiple goofs in task export_dict() methods.

--- src/rest/RestServer.py
+++ src/rest/RestServer.py
@@ -41,6 +41,45 @@ def tasks_avail():
     return json_resp(get_avail_tasks())
 
 
+@app.route('/equipment/heaters')
+def equipment_heaters():
+    return json_resp(get_heaters())
+
+
+@app.route('/equipment/vents')
+def equipment_vents():
+    return json_resp(get_vents())
+
+
+@app.route('/equipment/curtains')
+def equipment_curtains():
+    return json_resp(get_curtains())
+
+
+# I might currently think my earlier decision to make Sensors also
+# Subsystems and thereby lump them all in 'subsystems' was dumb
+# but we don't have to expose the sensors like other equipment to the
+# application integrators, so here's where we fix that.
+@app.route('/sensors/temp')
+def sensors_temp():
+    return json_resp(get_temps())
+
+
+@app.route('/sensors/rh')
+def sensors_rh():
+    return json_resp(get_rh_sensors())
+
+
+@app.route('/sensors/sun')
+def sensors_sun():
+    return json_resp(get_sun_sensors())
+
+
+@app.route('/sensors/wind')
+def sensors_wind():
+    return json_resp(get_wind_sensors())
+
+
 @app.route('/')
 def get_all():
     data = {}
@@ -48,34 +87,66 @@ def get_all():
     data['status'] = 'online'
     data['equipment'] = {}
     _eq = data['equipment']
-    _eq['vents'] = {}
-    _eq['heaters'] = {}
-    _eq['curtains'] = {}
-    _eq['temps'] = {}
-    _eq['rh_sensors'] = {}
-    _eq['sun_sensors'] = {}
-
-    for id, vent in eqfetch.get_vents().items():
-        _eq['vents'][id] = vent.export_dict()
-
-    for id, heater in eqfetch.get_heaters().items():
-        _eq['heaters'][id] = heater.export_dict()
-
-    for id, curtain in eqfetch.get_curtains().items():
-        _eq['curtains'][id] = curtain.export_dict()
-
-    for id, temp in eqfetch.get_temps().items():
-        _eq['temps'][id] = temp.export_dict()
-
-    for id, rh_sensor in eqfetch.get_rh_sensors().items():
-        _eq['rh_sensors'][id] = rh_sensor.export_dict()
-
-    for id, sun_sensor in eqfetch.get_sun_sensors().items():
-        _eq['sun_sensors'][id] = sun_sensor.export_dict()
+    _eq['vents'] = get_vents()
+    _eq['heaters'] = get_heaters()
+    _eq['curtains'] = get_curtains()
+    _eq['temps'] = get_temps()
+    _eq['rh_sensors'] = get_rh_sensors()
+    _eq['sun_sensors'] = get_sun_sensors()
+    _eq['wind_sensors'] = get_wind_sensors()
 
     data['running_tasks'] = get_running_tasks()
 
     return json_resp(data)
+
+
+def get_wind_sensors():
+    ret = {}
+    for id, wind_sensor in eqfetch.get_wind_sensors().items():
+        ret[id] = wind_sensor.export_dict()
+    return ret
+
+
+def get_sun_sensors():
+    ret = {}
+    for id, sun_sensor in eqfetch.get_sun_sensors().items():
+        ret[id] = sun_sensor.export_dict()
+    return ret
+
+
+def get_rh_sensors():
+    ret = {}
+    for id, rh_sensor in eqfetch.get_rh_sensors().items():
+        ret[id] = rh_sensor.export_dict()
+    return ret
+
+
+def get_temps():
+    ret = {}
+    for id, temp in eqfetch.get_temps().items():
+        ret[id] = temp.export_dict()
+    return ret
+
+
+def get_curtains():
+    ret = {}
+    for id, curtain in eqfetch.get_curtains().items():
+        ret[id] = curtain.export_dict()
+    return ret
+
+
+def get_vents():
+    ret = {}
+    for id, vent in eqfetch.get_vents().items():
+        ret[id] = vent.export_dict()
+    return ret
+
+
+def get_heaters():
+    ret = {}
+    for id, heater in eqfetch.get_heaters().items():
+        ret[id] = heater.export_dict()
+    return ret
 
 
 def get_running_tasks():
